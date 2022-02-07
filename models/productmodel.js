ff23a5a8e179ee36b42adbff66223d15dd4ff400
Kumar Kalyan <kum9748ar@gmail.com>
2022-02-07 23:44:28 +0530

    Added some models

--- models/productmodel.js
+++ models/productmodel.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
+    title: { type: String, required: true },
     description: { type: String, required: true },
-    stock: { type: Number, required: true }
-})
+    imageUrl: { type: String, required: true },
+    stock: { type: Number, required: true },
+    price: { type: Number },
+    color: { type: String },
+    price: { type: Number, required: true },
+    categories: { type: Array },
+}, { timestamps: true });
 module.exports = mongoose.model('Product', productSchema)
