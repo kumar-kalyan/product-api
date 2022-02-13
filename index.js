be95d811402fbd6a06098ac2ce034b6e5934523c
Kumar Kalyan <kum9748ar@gmail.com>
2022-02-13 22:27:06 +0530

    Fixed s bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/api/login', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/cart', orderRoutes)
+app.use('/api/cart', cartRoutes)
 
 
 app.listen(PORT, () => {
